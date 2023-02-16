22e13ac2dfa6ce2f1d20c90144b041447c397672
Muhammad Naeem <inaeem@protonmail.com>
2023-02-16 12:36:40 +0500

    scrape authors

--- qs/spiders/author.py
+++ qs/spiders/author.py
@@ -7,7 +7,23 @@ class AuthorSpider(scrapy.Spider):
     start_urls = ["http://quotes.toscrape.com/"]
 
     def parse(self, response):
-        pass
+
+        yield from response.follow_all(css=".author + a", callback=self.parse_author)
+
+        next_page = response.css('ul.page .next a::attr(href)').getall()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
 
     def parse_author(self, response):
-        pass
+        name = response.css('.author-title::text').get().strip()
+        dob = response.css('.author-born-date::text').get().strip()
+        dob_location = response.css(
+            '.author-born-location::text').get().strip()
+        about = response.css('.author-description::text').get().strip()
+
+        yield {
+            "dob": dob,
+            "name": name,
+            "about": about,
+            "dob_location": dob_location,
+        }
