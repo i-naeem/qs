e23e910affdcf6787e6ec7d368e76c618b2a568b
Muhammad Naeem <inaeem@protonmail.com>
2023-02-16 11:22:31 +0500

    add pagination scraping

--- qs/spiders/quotes.py
+++ qs/spiders/quotes.py
@@ -11,3 +11,8 @@ class QuotesSpider(scrapy.Spider):
 
         for quote_div in quote_divs:
             yield {"text": quote_div.css('span.text::text').get()}
+
+        next_page = response.css('ul.pager .next a::attr(href)').get()
+
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
