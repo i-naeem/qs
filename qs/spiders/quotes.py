9cfc970b9a1a4933cb28c8e70c60a58b7fb9422c
Muhammad Naeem <inaeem@protonmail.com>
2023-02-16 12:25:40 +0500

    add tags and author to scrape data

--- qs/spiders/quotes.py
+++ qs/spiders/quotes.py
@@ -4,13 +4,23 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+
+    def start_requests(self):
+        url = "http://quotes.toscrape.com/"
+        tag = getattr(self, 'tag', None)
+        if tag:
+            url = url + '/tag/' + tag
+
+        yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         quote_divs = response.css('div.quote')
 
         for quote_div in quote_divs:
-            yield {"text": quote_div.css('span.text::text').get()}
+            yield {"text": quote_div.css('span.text::text').get(),
+                   "tags": quote_div.css('.tag::text').getall(),
+                   "author": quote_div.css('.author::text').get()
+                   }
 
         next_page = response.css('ul.pager .next a::attr(href)').get()
 
